0606021c11666cee7e8487ce55e6b3203471afe5
Marko <markosokolich@gmail.com>
2015-06-06 13:32:42 -0700

    Updates to documentation
    Signed-off-by: Marko <markosokolich@gmail.com>

--- Plot1.R
+++ Plot1.R
@@ -1,26 +1,35 @@
 # Set the working directory to the location of the household power text file
 setwd("~/Learning/Johns Hopkins/R_Work/exdata-data-household_power_consumption")
 
-# I first read the data so that each line was a character string and then used head() to see how the data was organized
-# I could see a header with the column names and that the delimeter was ";"
+# I first read the data manually so that each line was a character string 
+# and then used head() to see how the data was organized
+# I could see a header with the column names and that the delimeter was ";" (European?)
 # This part of the exploration was removed from the script
-# Using read.csv2 (which has ";" as default delimeter) read the entire file to a data.frame called "data"
-# Data files with numeric values are corrupted unless colClasses = "character", columns are read as Factor unwanted.
+# Using read.csv2 (which has ";" as default delimeter) 
+# read the entire file to a data.frame called "data"
+# Data file reading will default to "Factor" even with numeric values 
+# It's a better practice to use colClasses = "character", all columns read as character.
+# The na.strings value was given in the problem as ?
 data <- read.csv2("household_power_consumption.txt", na.strings = "?", colClasses = "character")
 
-# Date is a character but we need to turn it into a  Date using as.Date and giving the appropriate format
-# The format is more difficult than it may seem capital "Y" indicates a 4-digit year
-# Small "m' indicates month as a 2 digit number,  Look at the data and look up the format keys before using as.Date
+# Date is a character but we need to turn it into a  Date 
+# using as.Date and giving the appropriate format
+# The format is more difficult than it may seem 
+# capital "Y" indicates a 4-digit year (small y a 2-digit year)
+# Small "m' indicates month as a 2 digit number,  
+# Look at the data and look up the format keys before using as.Date
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 # A subset data.frame called dataSub is obtained by limiting the data to only 2 dates
 dataSub <- subset(data,data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 
+# I ran into problems after several plots because mfrow does not reset
 # Set mfrow parameter to 1x1 (parameter reset in case this wasn't done previously)
 par(mfrow = c(1,1))
 
 # Plot a histogram in red (col = 2) with the title (main) and x label (xlab) as pre-defined
-xlab = "Global Active Power (kilowatts)"
+# Standard practice in EE is to capitalize the W in "Watts", abbreviation is kW
+xlab = "Global Active Power (kiloWatts)"
 main = "Global Active Power"
 hist(as.numeric(dataSub$Global_active_power), col = 2, main = main, xlab = xlab)
 
--- Plot3.R
+++ Plot3.R
@@ -1,27 +1,36 @@
 # Set the working directory to the location of the household power text file
 setwd("~/Learning/Johns Hopkins/R_Work/exdata-data-household_power_consumption")
 
-# I first read the data so that each line was a character string and then used head() to see how the data was organized
-# We could see a header with the column names and that the delimeter was ";"
+# I first read the data manually so that each line was a character string 
+# and then used head() to see how the data was organized
+# I could see a header with the column names and that the delimeter was ";" (European?)
 # This part of the exploration was removed from the script
-# Using read.csv2 (which has ";" as default delimeter) read the entire file to a df called "data"
-# Data files with numeric values are corrupted if colClasses = "character", columns are read as Factor unwanted.
+# Using read.csv2 (which has ";" as default delimeter) 
+# read the entire file to a data.frame called "data"
+# Data file reading will default to "Factor" even with numeric values 
+# It's a better practice to use colClasses = "character", all columns read as character.
+# The na.strings value was given in the problem as ?
 data <- read.csv2("household_power_consumption.txt", na.strings = "?", colClasses = "character")
 
-# Date is a character but we need to turn it into a  Date using as.Date and giving the appropriate format
-# The format is more difficult than it may seem capital "Y" indicates a 4-digit year
-# Small "m' indicates month as a 2 digit number,  Look at the data and look up the format keys before using as.Date
+# Date is a character but we need to turn it into a  Date 
+# using as.Date and giving the appropriate format
+# The format is more difficult than it may seem 
+# capital "Y" indicates a 4-digit year (small y a 2-digit year)
+# Small "m' indicates month as a 2 digit number,  
+# Look at the data and look up the format keys before using as.Date
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 # A subset data.frame called dataSub is obtained by limiting the data to only 2 dates
 dataSub <- subset(data,data$Date >= "2007-02-01" & data$Date <= "2007-02-02")
 
 # in data and dataSub the Date and Time are in 2 columns.  
-# We need to convert the 2 together to a single Date/Time column.  We create a second subset of data with this extra column
+# Need to convert the 2 together to a single Date/Time column.  
+# Create a second subset of data with this extra column
 # called "timestamp"
 dataSub2 <- within(dataSub, { timestamp=format(as.POSIXct(paste(Date, Time)), "%Y/%m/%d %H:%M:%S") })
 
-# Set mfrow parameter to 1x1 (parameter reset)
+# I ran into problems after several plots because mfrow does not reset
+# Set mfrow parameter to 1x1 (parameter reset in case this wasn't done previously)
 par(mfrow = c(1,1))
 
 # the xlab needs to be supressed in this plot so just make it blank
@@ -29,7 +38,7 @@ par(mfrow = c(1,1))
 xlab <- ""
 ylab = "Energy sub metering"
 
-# values were read in as factor and they need to be converted to numeric
+# values were read in as character and they need to be converted to numeric
 x2 <- as.POSIXct(dataSub2$timestamp)
 
 # This plot has 3 sets of y-data for the same x2 time series
@@ -45,8 +54,10 @@ plot(x2, y3, type = "l", ylab = ylab, xlab = xlab, col = 1)
 lines(x2, y4, type = "l", col = 2)
 lines(x2, y5, type = "l", col = 4)
 
-# Finally we add the legend to this plot. "topright" is the legend location, the next parameter are the labels
-# lwd is the line width (value 1 in this case) "col" is the colors of the three lines and text.col is the color of the labels
+# Finally we add the legend to this plot. 
+# "topright" is the legend location, the next parameter includes the labels in a vector
+# lwd is the line width (value 1 in this case) "col" are the colors of the three lines 
+# and text.col are the colors of the labels
 labels <- c("sub_metering_1", "sub_metering_2", "sub_metering_3")
 legend("topright", labels , lwd = 1, col = c(1,2,4), text.col = c(1,2,4), inset = c(0,0))
 
